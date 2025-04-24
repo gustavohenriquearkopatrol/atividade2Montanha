67dfad64af6393ad45de8dd8dfcb458ad3c6bdab
gustavohenriquearkpatrol <gustavohenriquearkpatrol@gmail.com>
2025-04-24 14:19:12 -0300

    primeiro

---
+++ atividade01/src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+       Scanner scanner = new Scanner(System.in);
+
+       int numero = 1;
+
+       while (numero <= 50){
+        System.out.println(numero);
+        
+        numero++;
+
+       }
+      scanner.close();
+}
+
+    }
+
+
---
+++ exerciciosomapositivo/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        double numero = 0;
+        double soma = 0;
+        
+
+        while(numero >= 0) {
+            System.out.println("Digite um número inteiro positivo (ou negativo para sair): ");
+            numero = scanner.nextInt();
+
+            if (numero <  0) {
+                System.out.println("Saindo do programa...");
+                break;
+            }
+
+            soma += numero;
+        }
+        
+        System.out.println(soma);
+        
+        
+
+
+
+
+}
+}
