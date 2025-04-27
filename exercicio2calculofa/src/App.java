8979a27d19a06d02dbb1d0e1472f012c556d077c
gustavohenriquearkpatrol <gustavohenriquearkpatrol@gmail.com>
2025-04-27 15:20:51 -0300

    exercicio2

---
+++ exercicio2calculofa/src/App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int numeros = 0;
+        int fatorial = 1;
+
+        System.out.println("Digite um número inteiro para cálculo: ");
+        numeros = scanner.nextInt();
+
+        if(numeros < 0 ){
+            System.out.println("Números negativos não são válidos");
+        }
+        else{
+
+        for(int i = 1; i <= numeros; i++ ){
+            fatorial *= i;
+        }      
+     
+
+     System.out.println(fatorial);
+    }
+}
+}
+
